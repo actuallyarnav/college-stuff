c4a8e6ef3db83dcd0d4fb3954e81d65999b21edf
arnav <arnavrathod@protonmail.com>
2024-08-11 00:40:21 +0530

    added some user options

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Introduction
+## Introduction
 This is a repository of the programs I write for college projects and/or assignments, or just stuff that I made with other college friends.
 Don't judge me lmao.
 
 I probably wouldn't have written this but it felt empty without a readme file so, yeah.
+
+# Programs
+## False Position/Regula Falsi
+The false position, or regula falsi method, is an iterative method used to find an approximate root of a mathematical function. This program does exactly that, albeit with a predefined function (user defined function is in progress)
--- falseposition.py
+++ falseposition.py
@@ -1,13 +1,13 @@
 '''
 Algorithm to automate the regula falsi method
 1. define the polynomial (done)
-2. get the value of epsilon from user (significant digits) (predefined but can do)
+2. get the value of epsilon from user (significant digits) (done)
 3. solve polynomial for f(x) starting from 0 (done)
 4. if adjacent values of f(x) are positive and negative, save the values of x0 and x1 (done as xp and xq)
 5. call the regula falsi method function and input the two values of x (done)
 6. return next value of x (done)
-7. check if error of the two values of x is less than epsilon. if not, goto step 5
-8. print final value of x
+7. check if error of the two values of x is less than epsilon. if not, goto step 5 (done)
+8. print final value of x (done)
 '''
 
 #importing the necessary libraries
@@ -20,24 +20,38 @@ def f(x):
 def regulafalsi(xp,xq):
     r = (xp*f(xq)-xq*f(xp))/(f(xq)-f(xp)) #xp is smaller value, xq is greater value
     return r
+print("predefined function is x**3-5*x+3")
 
 #defining minimum error
-error = eval(input("Enter the error (eg. 0.0005): "))
+decimalplaces = eval(input("How many decimal places should the answer be correct to?: "))
+try:
+    decimalplaces = int(decimalplaces) #converts input to int type
+except ValueError:
+    print("Invalid input! Please enter a valid integer.") #if it can't, it throws up an error
+error = (1/2)*(10**(- decimalplaces))
 
+#defining significant digits
+sd = input("How many significant digits should the answer contain?: ")
+try:
+    sd = int(sd)
+except ValueError:
+    print("Invalid input! Please enter a valid integer.")
 i=1
 while (f(i)*f(i+1)>=0):
     i=i+1
 xp=i
 xq=i+1
-print("Root lies between",xp, "and",xq)
-#print(f(xp))
-#print(f(xq))
-#print(regulafalsi(xp, xq))
-while(abs(xp-xq)>error):
+#perform approximations to find root
+while(abs(xp-xq)>error): #works until 2 adjacent roots have a difference less than the error
     xtemp=regulafalsi(xp,xq) #temp var to store a value of x, before assigning it to xp or xq
     if (f(xtemp)*f(xq)<0):
         xp=xtemp #changes the lower limit
     elif(f(xtemp)*f(xp)<0):
         xq=xtemp #changes the upper limit
 
-print("Approximate root is", xq)
+answer = f"{xq:.{sd}g}" #formatting the result to contain preferred amount of significant digits
+
+print("Approximate root is", answer)
+
+#work to do:
+#find a way to input a function, instead of predefining it in the code
