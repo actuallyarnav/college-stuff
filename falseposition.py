3e0c8a95f0ad1dbfeda20fd85d6e09fc05a28382
arnav <arnavrathod@protonmail.com>
2024-08-11 00:06:06 +0530

    false position code works

--- falseposition.py
+++ falseposition.py
@@ -1,28 +1,28 @@
 '''
 Algorithm to automate the regula falsi method
-1. define the polynomial
-2. get the value of epsilon from user (significant digits)
-3. solve polynomial for f(x) starting from 0
-4. if adjacent values of f(x) are positive and negative, save the values of x0 and x1
-5. call the regula falsi method function and input the two values of x
-6. return next value of x
+1. define the polynomial (done)
+2. get the value of epsilon from user (significant digits) (predefined but can do)
+3. solve polynomial for f(x) starting from 0 (done)
+4. if adjacent values of f(x) are positive and negative, save the values of x0 and x1 (done as xp and xq)
+5. call the regula falsi method function and input the two values of x (done)
+6. return next value of x (done)
 7. check if error of the two values of x is less than epsilon. if not, goto step 5
 8. print final value of x
 '''
+
 #importing the necessary libraries
 import math
 
 #defining polynomial function
 def f(x):
-    return x*math.log10(x)-1.2
-
+    return x**3-5*x+3
 #defining regula falsi function
 def regulafalsi(xp,xq):
-    r = (xp*f(xq)-xq*f(xp))/(f(xq)-f(xp))
+    r = (xp*f(xq)-xq*f(xp))/(f(xq)-f(xp)) #xp is smaller value, xq is greater value
     return r
 
 #defining minimum error
-error = 0.0005
+error = eval(input("Enter the error (eg. 0.0005): "))
 
 i=1
 while (f(i)*f(i+1)>=0):
@@ -30,8 +30,14 @@ while (f(i)*f(i+1)>=0):
 xp=i
 xq=i+1
 print("Root lies between",xp, "and",xq)
-print(f(xp))
-print(f(xq))
+#print(f(xp))
+#print(f(xq))
+#print(regulafalsi(xp, xq))
+while(abs(xp-xq)>error):
+    xtemp=regulafalsi(xp,xq) #temp var to store a value of x, before assigning it to xp or xq
+    if (f(xtemp)*f(xq)<0):
+        xp=xtemp #changes the lower limit
+    elif(f(xtemp)*f(xp)<0):
+        xq=xtemp #changes the upper limit
 
-print(regulafalsi(xp, xq))
-
+print("Approximate root is", xq)
